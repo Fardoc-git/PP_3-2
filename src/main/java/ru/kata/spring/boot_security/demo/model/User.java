b2359332e89eb50f75bca03d106682c9dbb9fea6
Fardoc-git <163406460+Fardoc-git@users.noreply.github.com>
2024-08-17 13:22:28 +0300

    1

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -32,8 +32,7 @@ public class User implements UserDetails {
     @Column
     private String password;
 
-    @Fetch(FetchMode.JOIN)
-    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
